aed8c9532456fc5d1a644195e747b7599f69c228
Tex <exponto73@gmail.com>
2022-02-09 10:38:46 +0100

    Maj
    Mise en place du background et création du svg.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Fonction qui colorise et réduit la navbar au scroll
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if(this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});
