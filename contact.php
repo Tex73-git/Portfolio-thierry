f6e268065c4e78434a439bc4309a8f49317a9274
Tex <exponto73@gmail.com>
2022-03-03 14:08:26 +0100

    Update all file

--- contact.php
+++ contact.php
@@ -1,6 +1,17 @@
 <?php
 
-$retour = mail("administratif@planete-digitale.fr", "essai", "coucou");
-if ($retour) {
-    echo "<p>le mail a bien été envoyez</p>";
-}
+    if (isset($_POST["message"])) {
+        $message = "Ce message vous a été envoyé via le formulaire de contact du site portfolio-thierry.fr
+        Name : " . $_POST["name"] . "
+        Firstname : " . $_POST["firstname"] . "
+        Email : " . $_POST["email"] . "
+        Sujet : " . $_POST["sujet"] . "
+        Message :" . $_POST["message"];
+
+        $retour = mail("administratif@planete-digitale.fr", $_POST["sujet"], $message,
+        "From:contact@portfolio-thierry.fr" . "\r\n" . "Reply-to:" . $_POST["email"]);
+
+        if ($retour) {
+            echo !alert() "<p>L'email a bien été envoyez !</p>";
+        }
+    }
